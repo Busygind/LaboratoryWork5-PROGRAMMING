256b77dea4e92a45671cf93c27adafaa8856e5b0
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-10 20:08:42 +0300

    Update Client.java

--- lab5/lab-client/src/main/java/com/lab5/client/Client.java
+++ lab5/lab-client/src/main/java/com/lab5/client/Client.java
@@ -1,6 +1,8 @@
 package com.lab5.client;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
@@ -9,13 +11,12 @@ public final class Client {
         throw new UnsupportedOperationException("This is an utility class and can not be instantiated");
     }
 
-    //TODO реализовать toString у всех классов, которые увидит пользователь
     public static void main(String[] args) throws IOException, InvocationTargetException, IllegalAccessException {
         XMLParser parser = new XMLParser();
-        CollectionOfDragons collection = new CollectionOfDragons();
-        FileInputStream file = new FileInputStream("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons.xml");
-        //TODO не забыть удалить эту хуйню
-        for (Dragon elem : parser.read(file)) {
+        FileInputStream inputFile = new FileInputStream("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons.xml");
+        FileOutputStream outputFile = new FileOutputStream("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons1.xml");
+        CollectionOfDragons collection = new CollectionOfDragons(outputFile, inputFile);
+        for (Dragon elem : parser.read(inputFile)) {
             collection.addDragon(elem);
         }
         CommandListener cl = new CommandListener(collection);
