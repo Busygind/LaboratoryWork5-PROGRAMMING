27520649b62725f50a5856feb652218e7a545e2e
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-10 20:08:44 +0300

    Update CollectionOfDragons.java

--- lab5/lab-client/src/main/java/com/lab5/client/CollectionOfDragons.java
+++ lab5/lab-client/src/main/java/com/lab5/client/CollectionOfDragons.java
@@ -1,24 +1,47 @@
 package com.lab5.client;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Date;
 import java.util.HashSet;
 
 public class CollectionOfDragons {
+    private static int idCounter = 1;
+    private FileInputStream inFile;
+    private FileOutputStream outFile;
     private HashSet<Dragon> dragons;
     private Date creationDate;
-//TODO проверить работает ли ссылка на общую коллекцию
-    public CollectionOfDragons() {
-        dragons = new HashSet<Dragon>();
+
+    public CollectionOfDragons(FileOutputStream outFile, FileInputStream inFile) {
+        this.outFile = outFile;
+        this.inFile = inFile;
+        dragons = new HashSet<>();
         creationDate = new Date();
     }
 
     public void addDragon(Dragon dragon) {
-        dragon.setId(dragons.size() + 1); //айди дракона зависит от текущего количества драконов
+        dragon.setId(idCounter); //айди дракона зависит от текущего количества драконов
+        idCounter++;
         dragons.add(dragon);
     }
 
     public void clear() {
         dragons.clear();
+    }
+
+    public void removeById(long id) {
+        for (Dragon dragon : dragons) {
+            if (dragon.getId() == id) {
+                dragons.remove(dragon);
+                System.out.println("Dragon succesfully removed");
+            }
+        }
+    }
+
+    public void showInfo() {
+        System.out.println("Collection type: " + dragons.getClass()
+                + " initialization date: " + creationDate
+                + " count of dragons: " + dragons.size());
     }
 
     public HashSet<Dragon> getDragons() {
@@ -29,9 +52,7 @@ public class CollectionOfDragons {
         return creationDate;
     }
 
-    public void showInfo() {
-        System.out.println("Collection type: " + dragons.getClass()
-                        + " initialization date: " + creationDate
-                        + " count of dragons: " + dragons.size());
+    public FileOutputStream getFile() {
+        return outFile;
     }
 }
