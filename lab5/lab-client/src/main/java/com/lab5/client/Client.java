494d302f8465b5d03d98f24bc4d589d296b9340d
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-11 16:49:15 +0300

    Update Client.java

--- lab5/lab-client/src/main/java/com/lab5/client/Client.java
+++ lab5/lab-client/src/main/java/com/lab5/client/Client.java
@@ -1,5 +1,6 @@
 package com.lab5.client;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,12 +14,21 @@ public final class Client {
     // TODO сделать javadoc
     // TODO реализовать обработку неверных команд пользователя
     public static void main(String[] args) throws IOException, InvocationTargetException, IllegalAccessException {
+        //TODO реализация проверки количества аргументов
+//        if (args.length == 0) {
+//            System.out.println("Неверное количество аргументов, попробуйте еще раз. Необходимый элемент: файл с расширением .xml");
+//            System.exit(0);
+//        }
+        
         XMLParser parser = new XMLParser();
-        FileInputStream inputFile = new FileInputStream("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons.xml");
+        File inputFile = new File("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons.xml");
         FileOutputStream outputFile = new FileOutputStream("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons1.xml");
-        CollectionOfDragons collection = new CollectionOfDragons(outputFile, inputFile);
-        for (Dragon elem : parser.read(inputFile)) {
+        CollectionOfDragons collection = new CollectionOfDragons(outputFile);
+        for (Dragon elem : parser.read(new FileInputStream(inputFile))) {
             collection.addDragon(elem);
+            if (elem.getCreationDate() == null) {
+                elem.setCreationDate();
+            }
         }
         CommandListener cl = new CommandListener(collection);
         cl.commandsReader();
