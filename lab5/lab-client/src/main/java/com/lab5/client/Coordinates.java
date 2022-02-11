78c7dc9eae8285089233c2db5fbb05cea09eae17
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-11 16:49:26 +0300

    Update Coordinates.java

--- lab5/lab-client/src/main/java/com/lab5/client/Coordinates.java
+++ lab5/lab-client/src/main/java/com/lab5/client/Coordinates.java
@@ -5,18 +5,23 @@ public class Coordinates {
     private Integer x; //Максимальное значение поля: 603, Поле не может быть null
     private float y;
     //TODO поправить конструктор
-    private Coordinates(Integer x, float y) {
+    public Coordinates(Integer x, float y) {
         this.x = x;
         this.y = y;
     }
 
-    public static Coordinates createInstance(Integer x, float y) {
-        if (x <= MAX_X_VALUE) {
-            return new Coordinates(x, y);
-        }
-        System.out.println("Координата x некорректна, не удалось инициализировать координаты");
-        return null;
+    public Coordinates() {
+
     }
+
+    //TODO создание экземпляра координат, если понадобится снова приватный конструктор
+//    public static Coordinates createInstance(Integer x, float y) {
+//        if (x <= MAX_X_VALUE) {
+//            return new Coordinates(x, y);
+//        }
+//        System.out.println("Координата x некорректна, не удалось инициализировать координаты");
+//        return null;
+//    }
 
     public Integer getX() {
         return this.x;
@@ -24,8 +29,7 @@ public class Coordinates {
 
     public void setX(Integer x) {
         if (x > MAX_X_VALUE) {
-            this.x = null;
-            System.out.println("Некорректное значение координаты дракона, введите данные о драконе заново");
+            throw new IllegalArgumentException("Некорретное значение координаты х");
         } else {
             this.x = x;
         }
