219035a8cdaa23dd56c126af200e38f08b7f7558
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-11 23:53:55 +0300

    Update Client.java

--- lab5/lab-client/src/main/java/com/lab5/client/Client.java
+++ lab5/lab-client/src/main/java/com/lab5/client/Client.java
@@ -6,6 +6,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ @author Dmitry Busygin
+ */
 public final class Client {
     private Client() {
         throw new UnsupportedOperationException("This is an utility class and can not be instantiated");
@@ -13,13 +16,18 @@ public final class Client {
 
     // TODO сделать javadoc
     // TODO реализовать обработку неверных команд пользователя
+
+    /**
+     * @throws IOException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     public static void main(String[] args) throws IOException, InvocationTargetException, IllegalAccessException {
         //TODO реализация проверки количества аргументов
 //        if (args.length == 0) {
 //            System.out.println("Неверное количество аргументов, попробуйте еще раз. Необходимый элемент: файл с расширением .xml");
 //            System.exit(0);
 //        }
-        
         XMLParser parser = new XMLParser();
         File inputFile = new File("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons.xml");
         FileOutputStream outputFile = new FileOutputStream("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWork5\\lab5\\lab-client\\src\\main\\Dragons1.xml");
