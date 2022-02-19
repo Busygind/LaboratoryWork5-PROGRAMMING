761314c7d2a015e4895c515964af24b94813b02e
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-20 00:33:29 +0300

    Update CollectionOfDragons.java

--- lab5/lab-client/src/main/java/com/lab5/client/CollectionOfDragons.java
+++ lab5/lab-client/src/main/java/com/lab5/client/CollectionOfDragons.java
@@ -1,57 +1,31 @@
 package com.lab5.client;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Date;
 import java.util.HashSet;
 
+/**
+ * Класс коллекции, содержащий текущую коллекцию <b>dragons</b>,
+ * отвечает за генерацию ID для новых элементов и за все действия,
+ * связанные с коллекцией.
+ */
 public class CollectionOfDragons {
 
     private static int idCounter = 1;
-    private FileOutputStream outFile;
+    private File outFile;
     private HashSet<Dragon> dragons;
     private Date creationDate;
-
-    public CollectionOfDragons(FileOutputStream outFile) {
-        this.outFile = outFile;
-        dragons = new HashSet<>();
-        creationDate = new Date();
-    }
 
     public CollectionOfDragons() {
         dragons = new HashSet<>();
         creationDate = new Date();
     }
 
-    /**
-     * @param dragon дракон, которого нужно добавить в коллекцию
-     */
-    public void addDragon(Dragon dragon) {
-        dragon.setId();
-        idCounter++;
-        dragons.add(dragon);
-        System.out.println("Дракон успешно добавлен в коллекцию");
-    }
-
-    public void clear() {
-        dragons.clear();
-    }
-
-    /**
-     * @param id id дракона, которого нужно удалить
-     */
-    public void removeById(long id) {
-        for (Dragon dragon : dragons) {
-            if (dragon.getId() == id) {
-                dragons.remove(dragon);
-                System.out.println("Dragon succesfully removed");
-            }
-        }
-    }
-
-    public void showInfo() {
-        System.out.println("Collection type: " + dragons.getClass()
-                + " initialization date: " + creationDate
-                + " count of dragons: " + dragons.size());
+    public CollectionOfDragons(File outFile) {
+        this.outFile = outFile;
+        dragons = new HashSet<>();
+        creationDate = new Date();
     }
 
     /**
@@ -71,11 +45,49 @@ public class CollectionOfDragons {
     /**
      * @return файл, в который производится запись готовой коллекции
      */
-    public FileOutputStream getFile() {
+    public File getOutFile() {
         return outFile;
     }
 
-    public void setOutFile(FileOutputStream outFile) {
+    public void setOutFile(File outFile) {
         this.outFile = outFile;
+    }
+
+    /**
+     * Метод, добавляющий полученного дракона в коллекцию
+     * @param dragon дракон, которого нужно добавить в коллекцию
+     */
+    public void addDragon(Dragon dragon) {
+        dragon.setId();
+        idCounter++;
+        dragons.add(dragon);
+        System.out.println("Дракон успешно добавлен в коллекцию");
+    }
+
+    public void clear() {
+        dragons.clear();
+    }
+
+    /**
+     * Метод, удаляющий дракона из коллекции по полученному ID,
+     * если таковой существует
+     * @param id id дракона, которого нужно удалить
+     */
+    public void removeById(long id) {
+        for (Dragon dragon : dragons) {
+            if (dragon.getId() == id) {
+                dragons.remove(dragon);
+                System.out.println("Dragon succesfully removed");
+            }
+        }
+    }
+
+    /**
+     * Метод, выводящий пользователю информацию о коллекции
+     */
+    public void showInfo() {
+        System.out.println("Collection type: " + dragons.getClass()
+                + " initialization date: " + creationDate
+                + " count of dragons: " + dragons.size());
     }
 }
