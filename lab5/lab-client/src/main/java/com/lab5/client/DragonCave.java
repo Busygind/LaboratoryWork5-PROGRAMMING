19ca34bb1f4ba55dc63826a9e1c765ad14de590a
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-21 01:56:27 +0300

    Update DragonCave.java

--- lab5/lab-client/src/main/java/com/lab5/client/DragonCave.java
+++ lab5/lab-client/src/main/java/com/lab5/client/DragonCave.java
@@ -1,7 +1,13 @@
 package com.lab5.client;
 
 public class DragonCave {
+    /**
+     * Глубина текущей пещеры
+     */
     private double depth;
+    /**
+     * Количество сокровищ в текущей пещере
+     */
     private int numberOfTreasures; //Значение поля должно быть больше 0
 
     public DragonCave(double depth, int numberOfTreasures) {
@@ -16,10 +22,26 @@ public class DragonCave {
 
     }
 
+    /**
+     * Метод, устанавливающий объекту пещеры глубину
+     *
+     * @param depth глубина пещеры, диапазон допустимых значений совпадает с диапазоном типа double
+     */
     public void setDepth(double depth) {
+        if (depth >= Double.MAX_VALUE) {
+            throw new IllegalArgumentException("Значение слишком большое, попробуйте снова");
+        }
+        if (depth <= Double.MIN_VALUE) {
+            throw new IllegalArgumentException("Значение слишком маленькое, попробуйте снова");
+        }
         this.depth = depth;
     }
 
+    /**
+     * Метод, устанавливающий объекту пещеры количество сокровищ
+     *
+     * @param numberOfTreasures количество сокровищ, диапазон допустимых значений (0; ]
+     */
     public void setNumberOfTreasures(int numberOfTreasures) {
         if (numberOfTreasures <= 0) {
             throw new IllegalArgumentException("Некорректное количество сокровищ в пещере, попробуйте снова");
@@ -27,10 +49,20 @@ public class DragonCave {
         this.numberOfTreasures = numberOfTreasures;
     }
 
+    /**
+     * Метод, возвращающий глубину текущей пещеры
+     *
+     * @return глубина пещеры
+     */
     public double getDepth() {
         return depth;
     }
 
+    /**
+     * Метод, возвращающий количество сокровищ в данной пещере
+     *
+     * @return количество сокровищ
+     */
     public int getNumberOfTreasures() {
         return numberOfTreasures;
     }
