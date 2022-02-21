1f9e849364cd70e08a9d367300fd8d303fae7b3e
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-21 22:22:31 +0300

    Update Command.java

--- lab5/lab-client/src/main/java/com/lab5/client/handlers/Command.java
+++ lab5/lab-client/src/main/java/com/lab5/client/handlers/Command.java
@@ -13,14 +13,34 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface Command {
 
+    /**
+     * Имя команды
+     * @return имя команды
+     */
     String name();
 
+    /**
+     * Список аргументов команды
+     * @return строка с аргументами
+     */
     String args();
 
+    /**
+     * Описание команды
+     * @return строка с описанием команды
+     */
     String desc();
 
+    /**
+     * Количество необходимых аргументов для данной команды
+     * @return количество аргументов
+     */
     int countOfArgs();
 
+    /**
+     * Возможные записи текущей команды, отличные от name
+     * @return массив возможных названий
+     */
     String[] aliases();
 
 }
